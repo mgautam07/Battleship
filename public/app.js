333615c19f4303de89f63649116d6e560377d7a0
Gautam <mgautam2k2@gmail.com>
2023-07-17 16:05:41 +0530

    socketio completed

--- public/app.js
+++ public/app.js
@@ -4,8 +4,214 @@ const flipButton = document.querySelector('#flip-button')
 const startButton = document.querySelector('#start-button')
 const turnDisplay = document.querySelector('#turn-display')
 const infoDisplay = document.querySelector('#info')
+const singlePlayerButton = document.querySelector('#singlePlayerButton')
+const multiPlayerButton = document.querySelector('#multiPlayerButton')
+let computerSquares = []
+let userSquares = []
+let currentPlayer = 'user'
+let gameMode = ""
+let playerNum = 0
+let ready = 0
+let enemyReady = false
+let shotFired = -1
 
-let angle = 0
+singlePlayerButton.addEventListener('click', startSinglePlayer)
+multiPlayerButton.addEventListener('click', startMultiPlayer)
+
+function startMultiPlayer(){
+  gameMode = "multiPlayer"
+
+  const blocks = document.querySelectorAll('#computer div')
+  for(let i=0; i<100; i++)
+  {
+    blocks[i].addEventListener('click', () => {
+      if(currentPlayer === 'user' && ready && enemyReady){
+        shotFired = i
+        socket.emit('fire', shotFired)
+      }
+    })
+    computerSquares.push(blocks[i])
+  }
+
+  function revealSquare(classList) {
+    const enemySquare = document.querySelectorAll('#computer div')[shotFired]
+    const obj = Object.values(classList)
+    console.log(enemySquare)
+    if (!enemySquare.classList.contains('boom') && currentPlayer === 'user' && !isGameOver) {
+      if (obj.includes('destroyer')) destroyerCount++
+      if (obj.includes('submarine')) submarineCount++
+      if (obj.includes('cruiser')) cruiserCount++
+      if (obj.includes('battleship')) battleshipCount++
+      if (obj.includes('carrier')) carrierCount++
+    }
+    if (obj.includes('taken')) {
+      enemySquare.classList.add('boom')
+    } else {
+      enemySquare.classList.add('empty')
+    }
+    checkForWins()
+    currentPlayer = 'enemy'
+  }
+
+  function enemyGo(square) {
+    if (gameMode === 'singlePlayer') square = Math.floor(Math.random() * userSquares.length)
+    if (!userSquares[square].classList.contains('boom')) {
+      userSquares[square].classList.add('boom')
+      if (userSquares[square].classList.contains('destroyer')) cpuDestroyerCount++
+      if (userSquares[square].classList.contains('submarine')) cpuSubmarineCount++
+      if (userSquares[square].classList.contains('cruiser')) cpuCruiserCount++
+      if (userSquares[square].classList.contains('battleship')) cpuBattleshipCount++
+      if (userSquares[square].classList.contains('carrier')) cpuCarrierCount++
+      checkForWins()
+    } else if (gameMode === 'singlePlayer') enemyGo()
+    currentPlayer = 'user'
+    turnDisplay.innerHTML = 'Your Go'
+  }
+
+  function checkForWins() {
+    let enemy = 'computer'
+    if(gameMode === 'multiPlayer') enemy = 'enemy'
+    if (destroyerCount === 2) {
+      infoDisplay.innerHTML = `You sunk the ${enemy}'s destroyer`
+      destroyerCount = 10
+    }
+    if (submarineCount === 3) {
+      infoDisplay.innerHTML = `You sunk the ${enemy}'s submarine`
+      submarineCount = 10
+    }
+    if (cruiserCount === 3) {
+      infoDisplay.innerHTML = `You sunk the ${enemy}'s cruiser`
+      cruiserCount = 10
+    }
+    if (battleshipCount === 4) {
+      infoDisplay.innerHTML = `You sunk the ${enemy}'s battleship`
+      battleshipCount = 10
+    }
+    if (carrierCount === 5) {
+      infoDisplay.innerHTML = `You sunk the ${enemy}'s carrier`
+      carrierCount = 10
+    }
+    if (cpuDestroyerCount === 2) {
+      infoDisplay.innerHTML = `${enemy} sunk your destroyer`
+      cpuDestroyerCount = 10
+    }
+    if (cpuSubmarineCount === 3) {
+      infoDisplay.innerHTML = `${enemy} sunk your submarine`
+      cpuSubmarineCount = 10
+    }
+    if (cpuCruiserCount === 3) {
+      infoDisplay.innerHTML = `${enemy} sunk your cruiser`
+      cpuCruiserCount = 10
+    }
+    if (cpuBattleshipCount === 4) {
+      infoDisplay.innerHTML = `${enemy} sunk your battleship`
+      cpuBattleshipCount = 10
+    }
+    if (cpuCarrierCount === 5) {
+      infoDisplay.innerHTML = `${enemy} sunk your carrier`
+      cpuCarrierCount = 10
+    }
+
+    if ((destroyerCount + submarineCount + cruiserCount + battleshipCount + carrierCount) === 50) {
+      infoDisplay.innerHTML = "YOU WIN"
+      gameOver()
+    }
+    if ((cpuDestroyerCount + cpuSubmarineCount + cpuCruiserCount + cpuBattleshipCount + cpuCarrierCount) === 50) {
+      infoDisplay.innerHTML = `${enemy.toUpperCase()} WINS`
+      gameOver()
+    }
+  }
+
+  function gameOver() {
+    isGameOver = true
+  }
+
+  const temp = document.querySelectorAll('#player div')
+  for(let i=0; i<100; i++)
+  {
+    userSquares.push(temp[i])
+  }
+
+  const socket = io()
+  socket.on('player-number', num => {
+    if(num === -1){
+      infoDisplay.textContent = "Server full"
+    }
+    else{
+      playerNum = parseInt(num)
+      if(playerNum === 1) currentPlayer = "enemy"
+      console.log(playerNum)
+      socket.emit('check-players')
+    }
+  })
+
+  socket.on('player-connection', num => {
+    console.log(`Player number ${num} has disconnected or connected`)
+    playerConnectedOrDisconnected(num)
+  })
+
+  socket.on('enemy-ready', num => {
+    enemyReady = true
+    playerReady(num)
+    if(ready) startGameMulti(socket)
+  })
+
+  socket.on('check-players', players => {
+    players.forEach((p, i) => {
+      if(p.connected) playerConnectedOrDisconnected(i)
+      if(p.ready){
+        playerReady(i)
+        if(i !== playerNum) enemyReady = true
+      }
+    })
+  })
+
+  // On fire receive
+  socket.on('fire', id => {
+    console.log('fire', id)
+    enemyGo(id)
+    const square = userSquares[id].classList
+    console.log(square)
+    socket.emit('fire-reply', square)
+    startGameMulti(socket)
+  })
+
+  socket.on('fire-reply', square => {
+    console.log(square)
+    revealSquare(square)
+    startGameMulti(socket)
+  })
+  
+  socket.on('timeout', () => {
+    infoDisplay.textContent = 'Reached 10 min limit'
+  })
+
+  startButton.addEventListener('click', () => {
+    flipButton.replaceWith(flipButton.cloneNode(true))
+    startGameMulti(socket)
+  })
+
+  // comp
+
+  function playerConnectedOrDisconnected(num) {
+    let player = `.p${parseInt(num) + 1}`
+    document.querySelector(`${player} .connected span`).classList.toggle('green')
+    if(parseInt(num) === playerNum)
+      document.querySelector(player).style.fontWeight = 'bold'
+  }
+}
+
+function startSinglePlayer() {
+  gameMode = "singlePlayer"
+  addShipPiece(destroyer)
+  addShipPiece(submarine)
+  addShipPiece(cruiser)
+  addShipPiece(battleship)
+  addShipPiece(carrier)
+
+  startButton.addEventListener('click', startGameSingle)
+
+}
 
 function flip(){
   // const optionShips = Array.from(optionContainer.children)
@@ -123,11 +329,6 @@ function addShipPiece(ship){
     addShipPiece(ship)
   }
 }
-addShipPiece(destroyer)
-addShipPiece(submarine)
-addShipPiece(cruiser)
-addShipPiece(battleship)
-addShipPiece(carrier)
 
 function addPlayerShipPiece(ship){
   const allPlayerBoardBlocks = document.querySelectorAll('#player div')
@@ -172,12 +373,40 @@ addPlayerShipPiece(cruiser)
 addPlayerShipPiece(battleship)
 addPlayerShipPiece(carrier)
 
-let gameOver = false
+let isGameOver = false
 let playerTurn
 
-startButton.addEventListener('click', startGame)
 
-function startGame() {
+//Game logic for multi player
+function startGameMulti(socket) {
+  // console.log(isGameOver)
+  if(isGameOver) return
+  if(!ready){
+    console.log('ready')
+    socket.emit('player-ready')
+    ready = true
+    playerReady(playerNum)
+  }
+  if(enemyReady)
+  {
+    if(currentPlayer === 'user'){
+      turnDisplay.textContent = 'Your turn'
+    }
+    else if(currentPlayer === 'enemy'){
+      turnDisplay.textContent = "Enemy's turn"
+    }
+  }
+}
+
+function playerReady(num) {
+  console.log(num)
+  let player = `.p${parseInt(num)+1}`
+  console.log(player)
+  document.querySelector(`${player} .ready span`).classList.toggle('green')
+}
+
+// Game logic for single player
+function startGameSingle() {
   if (playerTurn === undefined) {
     const allBoardBlocks = document.querySelectorAll('#computer div')
     allBoardBlocks.forEach(block => block.addEventListener('click', handleClick))
@@ -194,7 +423,7 @@ let playerSunkShips = []
 let computerSunkShips = []
 
 function handleClick(e) {
-  if(!gameOver){
+  if(!isGameOver){
     if(e.target.classList.contains('taken')){
       e.target.classList.add('boom')
       infoDisplay.textContent = 'You hit computers ship'
@@ -213,28 +442,39 @@ function handleClick(e) {
     playerTurn = false
     const allBoardBlocks = document.querySelectorAll('#computer div')
     allBoardBlocks.forEach(block => block.replaceWith(block.cloneNode(true)))
-    setTimeout(computerGo, 2000)
+    setTimeout(enemyGo, 2000)
   }
 }
 
-function computerGo() {
-  if(!gameOver){
-    turnDisplay.textContent = 'Computers turn'
-    infoDisplay.textContent = 'Computer thinking'
+let cpuDestroyerCount = 0
+let cpuSubmarineCount = 0
+let cpuCruiserCount = 0
+let cpuBattleshipCount = 0
+let cpuCarrierCount = 0
+let destroyerCount = 0
+let submarineCount = 0
+let cruiserCount = 0
+let battleshipCount = 0
+let carrierCount = 0
+
+function enemyGo(square) {
+  if(!isGameOver){
+    turnDisplay.textContent = 'Enemy turn'
+    infoDisplay.textContent = 'Enemy thinking'
 
     setTimeout(() =>{
-      let randomGo = Math.floor(Math.random() * width * width)
+      if(gameMode === 'singlePlayer') square = Math.floor(Math.random() * width * width)
       const allBoardBlocks = document.querySelectorAll('#player div')
-      if(allBoardBlocks[randomGo].classList.contains('taken') && 
-          allBoardBlocks[randomGo].classList.contains('boom')){
-          computerGo()
+      if(allBoardBlocks[square].classList.contains('taken') && 
+          allBoardBlocks[square].classList.contains('boom')){
+          enemyGo(square)
           return
       }
-      else if(allBoardBlocks[randomGo].classList.contains('taken') && 
-              !allBoardBlocks[randomGo].classList.contains('boom')){
-        allBoardBlocks[randomGo].classList.add('boom')
-        infoDisplay.textContent = 'Computer hit your ship'
-        let classes = Array.from(allBoardBlocks[randomGo].classList)
+      else if(allBoardBlocks[square].classList.contains('taken') && 
+              !allBoardBlocks[square].classList.contains('boom')){
+        allBoardBlocks[square].classList.add('boom')
+        infoDisplay.textContent = 'Enemy hit your ship'
+        let classes = Array.from(allBoardBlocks[square].classList)
         classes = classes.filter(className => className !== 'block')
         classes = classes.filter(className => className !== 'boom')
         classes = classes.filter(className => className !== 'taken')
@@ -242,8 +482,8 @@ function computerGo() {
         checkScore('computer', computerHits, computerSunkShips)
       }
       else {
-        infoDisplay.textContent = 'Computer missed'
-        allBoardBlocks[randomGo].classList.add('empty')
+        infoDisplay.textContent = 'Enemy missed'
+        allBoardBlocks[square].classList.add('empty')
       }
     }, 2000)
 
@@ -279,10 +519,10 @@ function checkScore(user, userHits, userSunkShips) {
   // console.log('playerSS', playerSunkShips)
   if(playerSunkShips.length === 5){
     infoDisplay.textContent = 'You sunk all the computers ships'
-    gameOver = true
+    isGameOver = true
   }
   else if(computerSunkShips.length === 5){
     infoDisplay.textContent = 'Computer sunk all your ships'
-    gameOver = true
+    isGameOver = true
   }
 }
---
+++ server.js
@@ -0,0 +1,72 @@
+const express = require('express')
+const path = require('path')
+const http = require('http')
+const socketio = require('socket.io')
+
+const app = express()
+const port = process.env.PORT | 3000
+const server = http.createServer(app)
+const io = socketio(server)
+
+app.use(express.static('public'))
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+//socket work
+let connections = [null, null]
+io.on('connection', socket => {
+  let playerIndex = -1
+  for(const i in connections){
+    if(connections[i] === null){
+      playerIndex = i
+      break
+    }
+  }
+  socket.emit('player-number', playerIndex)
+
+  console.log(`Player ${playerIndex} has connected`)
+  //ignoring player 3
+  if(playerIndex === -1)  return
+
+  connections[playerIndex] = false
+
+  socket.broadcast.emit('player-connection', playerIndex)
+  socket.on('disconnect', () => {
+    console.log(`Player ${playerIndex} disconnected`)
+    connections[playerIndex] = null
+    socket.broadcast.emit('player-connection', playerIndex)
+  })
+
+  socket.on('player-ready', () => {
+    socket.broadcast.emit('enemy-ready', playerIndex)
+    connections[playerIndex] = true
+  })
+
+  socket.on('check-players', () => {
+    const players = []
+    for(const i in connections)
+      connections[i] === null ? players.push({connected: false, ready: false}) : players.push({connected: true, ready: connections[i]})
+    socket.emit('check-players', players)
+  })
+
+  socket.on('fire', id => {
+    console.log(`Shot fired from ${playerIndex}`, id)
+    socket.broadcast.emit('fire', id)
+  })
+
+  socket.on('fire-reply', square =>{
+    console.log('fire-reply', square)
+    socket.broadcast.emit('fire-reply', square)
+  })
+
+  setTimeout(() => {
+    connections[playerIndex] = null
+    socket.emit('timeout')
+    socket.disconnect()
+  }, 600000)
+})
+server.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
